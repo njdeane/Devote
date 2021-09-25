4561f5b2729590222f010a8b1d20b69364774b46
Nic Deane <njdeane7@gmail.com>
2021-09-25 17:15:23 +1000

    Add comments and clean up template core data code.

--- Devote/Model/Persistence.swift
+++ Devote/Model/Persistence.swift
@@ -8,48 +8,40 @@
 import CoreData
 
 struct PersistenceController {
-    static let shared = PersistenceController()
-
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
-
-    let container: NSPersistentContainer
-
-    init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "Devote")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                Typical reasons for an error here include:
-                * The parent directory does not exist, cannot be created, or disallows writing.
-                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                * The device is out of space.
-                * The store could not be migrated to the current model version.
-                Check the error message to determine what the actual problem was.
-                */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
+  
+  // MARK: - 1. PERSISTENT CONTROLLER (Singleton for entire app to use)
+  static let shared = PersistenceController()
+  
+  // MARK: - 2. PERSISTENT CONTAINTER (Storage container)
+  let container: NSPersistentContainer
+  
+  // MARK: - 3. INITIALIZATION (Load the persistent store)
+  init(inMemory: Bool = false) {
+    container = NSPersistentContainer(name: "Devote")
+    if inMemory {
+      container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null") // This is temporary memory (Good for testing)
     }
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in // This is persistent storage (SQLite store)
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+  }
+  
+  // MARK: - 4. PREVIEW (Test config for SwiftUI Preview)
+  static var preview: PersistenceController = {
+    let result = PersistenceController(inMemory: true)
+    let viewContext = result.container.viewContext
+    for _ in 0..<10 {
+      let newItem = Item(context: viewContext)
+      newItem.timestamp = Date()
+    }
+    do {
+      try viewContext.save()
+    } catch {
+      let nsError = error as NSError
+      fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+    }
+    return result
+  }()
 }
