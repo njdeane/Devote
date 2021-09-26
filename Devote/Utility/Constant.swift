00b4fbeb4581a9d21df16bf68a17e9a14ff988a0
Nic Deane <njdeane7@gmail.com>
2021-09-26 10:15:12 +1000

    Add background gradient and image.

--- Devote/Utility/Constant.swift
+++ Devote/Utility/Constant.swift
@@ -16,5 +16,8 @@ let itemFormatter: DateFormatter = {
 }()
 
 // UI
+var backgroundGradient: LinearGradient {
+  return LinearGradient(gradient: Gradient(colors: [Color.pink, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+}
 
 // UX
--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -62,46 +62,55 @@ struct ContentView: View {
   // MARK: - BODY
   var body: some View {
     NavigationView {
-      VStack {
-        VStack(spacing: 16) {
-          TextField("New Task", text: $task)
+      ZStack {
+        VStack {
+          VStack(spacing: 16) {
+            TextField("New Task", text: $task)
+              .padding()
+              .background(
+                Color(UIColor.systemGray6)
+              )
+              .cornerRadius(10)
+            
+            Button(action: {
+              addItem()
+            }, label: {
+              Spacer()
+              Text("Save")
+              Spacer()
+            })
+            .disabled(isButtonDisabled) // this uses computed property.. remember this!
             .padding()
-            .background(
-              Color(UIColor.systemGray6)
-            )
+            .font(.headline)
+            .foregroundColor(.white)
+            .background(isButtonDisabled ? Color.gray : Color.pink)
             .cornerRadius(10)
-          
-          Button(action: {
-            addItem()
-          }, label: {
-            Spacer()
-            Text("Save")
-            Spacer()
-          })
-          .disabled(isButtonDisabled) // this uses computed property.. remember this!
-          .padding()
-          .font(.headline)
-          .foregroundColor(.white)
-          .background(isButtonDisabled ? Color.gray : Color.pink)
-          .cornerRadius(10)
-        } //: VSTACK
-        .padding()
-        
-        List {
-          ForEach(items) { item in
-            VStack(alignment: .leading) {
-              Text(item.task ?? "")
-                .font(.headline)
-                .fontWeight(.bold)
-              
-              Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                .font(.footnote)
-                .foregroundColor(.gray)
-            } //: LIST
           }
-          .onDelete(perform: deleteItems)
-        } //: LIST
-      } //: VSTACK
+          .padding()
+          
+          List {
+            ForEach(items) { item in
+              VStack(alignment: .leading) {
+                Text(item.task ?? "")
+                  .font(.headline)
+                  .fontWeight(.bold)
+                
+                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                  .font(.footnote)
+                  .foregroundColor(.gray)
+              }
+            }
+            .onDelete(perform: deleteItems)
+          }
+          .listStyle(InsetGroupedListStyle())
+          .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
+          .padding(.vertical, 0)
+          .frame(maxWidth: 640)
+        }
+      }
+      .onAppear() {
+        UITableView.appearance().backgroundColor = UIColor.clear
+      }
       .navigationBarTitle("Daily Tasks", displayMode: .large)
       .toolbar {
         #if os(iOS)
@@ -109,8 +118,15 @@ struct ContentView: View {
           EditButton()
         }
         #endif
-      } //: TOOLBAR
-    } //: NAVIGATION
+      }
+      .background(
+        BackgroundImageView()
+      )
+      .background(
+        backgroundGradient.ignoresSafeArea()
+      )
+    }
+    .navigationViewStyle(StackNavigationViewStyle())
   }
 }
 
